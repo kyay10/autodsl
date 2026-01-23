2f62b633dee8dde706a3fe527600160b8afd07fd
Youssef Shoaib <canonballt@gmail.com>
2026-01-23 09:33:19 +0000

    Implement resolver by delegation

--- processor/src/main/kotlin/com/faendir/kotlin/autodsl/DslGenerator.kt
+++ processor/src/main/kotlin/com/faendir/kotlin/autodsl/DslGenerator.kt
@@ -3,20 +3,42 @@ package com.faendir.kotlin.autodsl
 import com.faendir.kotlin.autodsl.parameter.Parameter
 import com.faendir.kotlin.autodsl.parameter.ParameterFactory
 import com.google.devtools.ksp.symbol.ClassKind
-import com.squareup.kotlinpoet.*
-import io.github.enjoydambience.kotlinbard.*
-import java.util.*
+import com.squareup.kotlinpoet.AnnotationSpec
+import com.squareup.kotlinpoet.BOOLEAN
+import com.squareup.kotlinpoet.BYTE
+import com.squareup.kotlinpoet.CHAR
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.DOUBLE
+import com.squareup.kotlinpoet.FLOAT
+import com.squareup.kotlinpoet.INT
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.LONG
+import com.squareup.kotlinpoet.ParameterizedTypeName
+import com.squareup.kotlinpoet.SHORT
+import com.squareup.kotlinpoet.WildcardTypeName
+import com.squareup.kotlinpoet.asClassName
+import com.squareup.kotlinpoet.joinToCode
+import io.github.enjoydambience.kotlinbard.TypeSpecBuilder
+import io.github.enjoydambience.kotlinbard.addAnnotation
+import io.github.enjoydambience.kotlinbard.addClass
+import io.github.enjoydambience.kotlinbard.addFunction
+import io.github.enjoydambience.kotlinbard.addProperty
+import io.github.enjoydambience.kotlinbard.buildFile
+import io.github.enjoydambience.kotlinbard.codeFmt
+import io.github.enjoydambience.kotlinbard.nullable
+import org.jetbrains.kotlin.utils.addToStdlib.ifNotEmpty
+import java.util.Locale
 import kotlin.jvm.internal.DefaultConstructorMarker
 import kotlin.properties.Delegates
 
 const val DEFAULTS_BITFLAGS_FIELD_NAME = "_defaultsBitField"
 
-class DslGenerator<A, T : A, C : A>(
+class DslGenerator<A, T : A, C : A, P : A>(
     private val kotlinVersion: KotlinVersion,
     private val logger: Logger<T>,
     private val codeGenerator: CodeWriter<T>,
-    private val resolver: SourceInfoResolver<A, T, C, *>
-) {
+    resolver: SourceInfoResolver<A, T, C, P>
+) : SourceInfoResolver<A, T, C, P> by resolver {
     private val parameterFactory = ParameterFactory(resolver)
 
     private fun error(reason: String, clazz: T) {
@@ -24,36 +46,29 @@ class DslGenerator<A, T : A, C : A>(
     }
 
 
-    fun process(): List<T> {
-        return resolver.getClassesWithAnnotation(AutoDsl::class)
-            .flatMap { processClass(it, resolver.run { it.getAnnotationTypeProperty(AutoDsl::class, AutoDsl::dslMarker) }) }
+    fun process(): List<T> = getClassesWithAnnotation(AutoDsl::class).flatMap {
+        processClass(it, it.getAnnotationTypeProperty(AutoDsl::class, AutoDsl::dslMarker))
     }
 
-    private fun processClass(clazz: T, markerType: ClassName?): List<T> = resolver.run {
+    private fun processClass(clazz: T, markerType: ClassName?): List<T> {
         when (clazz.getClassKind()) {
             ClassKind.INTERFACE -> error("must not be an interface", clazz)
             ClassKind.ENUM_CLASS -> error("must not be an enum class", clazz)
             ClassKind.ENUM_ENTRY -> error("must not be an enum entry", clazz)
             ClassKind.OBJECT -> error("must not be an object", clazz)
-            ClassKind.ANNOTATION_CLASS -> {
-                val deferred = resolver.getClassesWithAnnotation(clazz).flatMap { processClass(it, markerType) }
-                if (deferred.isNotEmpty()) {
-                    return deferred + clazz
-                }
-            }
-            ClassKind.CLASS -> {
-                if (!generate(clazz, markerType)) {
-                    return listOf(clazz)
-                }
-            }
+            ClassKind.ANNOTATION_CLASS -> getClassesWithAnnotation(clazz)
+                .flatMap { processClass(it, markerType) }
+                .ifNotEmpty { return this + clazz }
+
+            ClassKind.CLASS -> if (!generate(clazz, markerType)) return listOf(clazz)
         }
-        emptyList()
+        return emptyList()
     }
 
     /**
      * returns true if class generation was successful
      */
-    private fun generate(clazz: T, markerType: ClassName?): Boolean = resolver.run {
+    private fun generate(clazz: T, markerType: ClassName?): Boolean {
         if (clazz.isAbstract()) {
             error("must not be abstract", clazz)
             return false
@@ -99,9 +114,13 @@ class DslGenerator<A, T : A, C : A>(
                 addFunction("build") {
                     returns(type)
                     parameters.filter { it.isMandatory }.groupBy { it.group }.forEach { (_, parameters) ->
-                        addStatement("check(%L)·{ \"%L·must·be·assigned.\" }",
+                        addStatement(
+                            "check(%L)·{ \"%L·must·be·assigned.\" }",
                             parameters.map { "%L != null".codeFmt(it.name) }.joinToCode(" || "),
-                            parameters.joinToString(separator = ",·", prefix = if (parameters.size > 1) "One·of·" else "") { it.name }
+                            parameters.joinToString(
+                                separator = ",·",
+                                prefix = if (parameters.size > 1) "One·of·" else ""
+                            ) { it.name }
                         )
                     }
                     if (parameters.any { it.hasDefault }) {
@@ -109,24 +128,31 @@ class DslGenerator<A, T : A, C : A>(
                             "return %T::class.java.getConstructor(%L, %L, %L).newInstance(%L, %L, null)",
                             type,
                             parameters.map {
-                                "%T::class.%L".codeFmt(it.typeName.toRawType().nonnull, if (it.typeName.isNullable) "javaObjectType" else "java")
+                                "%T::class.%L".codeFmt(
+                                    it.typeName.toRawType().nonnull,
+                                    if (it.typeName.isNullable) "javaObjectType" else "java"
+                                )
                             }.joinToCode(),
                             bitFieldIndices.map { "%T::class.java".codeFmt(INT) }.joinToCode(),
-                            if (kotlinVersion.isAtLeast(1, 5)) "%T::class.java".codeFmt(DefaultConstructorMarker::class.asClassName())
+                            if (kotlinVersion.isAtLeast(
+                                    1,
+                                    5
+                                )
+                            ) "%T::class.java".codeFmt(DefaultConstructorMarker::class.asClassName())
                             else "Class.forName(%S)".codeFmt(DefaultConstructorMarker::class.java.name),
                             parameters.map {
-                                    when {
-                                        it.typeName.isNullable -> "%L"
-                                        it.typeName == BOOLEAN -> "%L ?: false"
-                                        it.typeName == BYTE -> "%L ?: 0"
-                                        it.typeName == SHORT -> "%L ?: 0"
-                                        it.typeName == INT -> "%L ?: 0"
-                                        it.typeName == LONG -> "%L ?: 0"
-                                        it.typeName == CHAR -> "%L ?: '\\u0000'"
-                                        it.typeName == FLOAT -> "%L ?: 0.0f"
-                                        it.typeName == DOUBLE -> "%L ?: 0.0"
-                                        else -> "%L"
-                                    }.codeFmt(it.name)
+                                when {
+                                    it.typeName.isNullable -> "%L"
+                                    it.typeName == BOOLEAN -> "%L ?: false"
+                                    it.typeName == BYTE -> "%L ?: 0"
+                                    it.typeName == SHORT -> "%L ?: 0"
+                                    it.typeName == INT -> "%L ?: 0"
+                                    it.typeName == LONG -> "%L ?: 0"
+                                    it.typeName == CHAR -> "%L ?: '\\u0000'"
+                                    it.typeName == FLOAT -> "%L ?: 0.0f"
+                                    it.typeName == DOUBLE -> "%L ?: 0.0"
+                                    else -> "%L"
+                                }.codeFmt(it.name)
                             }.joinToCode(),
                             bitFieldIndices.map { "%L".codeFmt(DEFAULTS_BITFLAGS_FIELD_NAME + it) }.joinToCode()
                         )
@@ -134,7 +160,8 @@ class DslGenerator<A, T : A, C : A>(
                         addStatement(
                             "return %T(%L)",
                             type,
-                            parameters.map { (if (it.typeName.isNullable) "%L" else "%L!!").codeFmt(it.name) }.joinToCode()
+                            parameters.map { (if (it.typeName.isNullable) "%L" else "%L!!").codeFmt(it.name) }
+                                .joinToCode()
                         )
                     }
                 }
@@ -161,20 +188,30 @@ class DslGenerator<A, T : A, C : A>(
         returns(parameter.typeName.nonnull)
     }
 
-    private fun TypeSpecBuilder.addParameterNestedAdder(parameter: Parameter) = addFunction(parameter.collectionType!!.singular) {
-        val elementType = (parameter.typeName as ParameterizedTypeName).typeArguments.first()
-        val nestedBuilderType = elementType.withBuilderSuffix()
-        addParameter("initializer", nestedBuilderType.asLambdaReceiver())
-        addStatement("val result = %T().apply(initializer).build()", nestedBuilderType)
-        addStatement("%1L = %1L?.plus(result) ?: %2L(result)", parameter.name, parameter.collectionType.createFunction)
-        addStatement("return result")
-        returns(elementType.nonnull)
-    }
+    private fun TypeSpecBuilder.addParameterNestedAdder(parameter: Parameter) =
+        addFunction(parameter.collectionType!!.singular) {
+            val elementType = (parameter.typeName as ParameterizedTypeName).typeArguments.first()
+            val nestedBuilderType = elementType.withBuilderSuffix()
+            addParameter("initializer", nestedBuilderType.asLambdaReceiver())
+            addStatement("val result = %T().apply(initializer).build()", nestedBuilderType)
+            addStatement(
+                "%1L = %1L?.plus(result) ?: %2L(result)",
+                parameter.name,
+                parameter.collectionType.createFunction
+            )
+            addStatement("return result")
+            returns(elementType.nonnull)
+        }
 
-    private fun TypeSpecBuilder.addParameterBuilderStyleVarargSetter(parameter: Parameter, builderType: ClassName, type: ClassName) =
+    private fun TypeSpecBuilder.addParameterBuilderStyleVarargSetter(
+        parameter: Parameter,
+        builderType: ClassName,
+        type: ClassName
+    ) =
         addFunction("with${parameter.name.replaceFirstChar { it.uppercase() }}") {
             val elementType =
-                (parameter.typeName as ParameterizedTypeName).typeArguments.first().let { if (it is WildcardTypeName) it.outTypes.first() else it }
+                (parameter.typeName as ParameterizedTypeName).typeArguments.first()
+                    .let { if (it is WildcardTypeName) it.outTypes.first() else it }
             returns(builderType)
             addParameter(parameter.name, elementType, KModifier.VARARG)
             addStatement("this.%1L·= %1L.%2L()", parameter.name, parameter.collectionType!!.convertFunction)
@@ -183,7 +220,11 @@ class DslGenerator<A, T : A, C : A>(
             addKdoc("@see %T.%L", type, parameter.name)
         }
 
-    private fun TypeSpecBuilder.addParameterBuilderStyleSetter(parameter: Parameter, builderType: ClassName, type: ClassName) =
+    private fun TypeSpecBuilder.addParameterBuilderStyleSetter(
+        parameter: Parameter,
+        builderType: ClassName,
+        type: ClassName
+    ) =
         addFunction("with${parameter.name.replaceFirstChar { it.uppercase() }}") {
             returns(builderType)
             addParameter(parameter.name, parameter.typeName)
@@ -193,26 +234,27 @@ class DslGenerator<A, T : A, C : A>(
             addKdoc("@see %T.%L", type, parameter.name)
         }
 
-    private fun TypeSpecBuilder.addParameterProperty(parameter: Parameter, type: ClassName) = addProperty(parameter.name, parameter.typeName.nullable) {
-        mutable(true)
-        if (parameter.hasDefault) {
-            delegate(
-                "%1T.observable(null)·{·_, _, _·-> %2L·= %2L and %3L }",
-                Delegates::class.asClassName(),
-                DEFAULTS_BITFLAGS_FIELD_NAME + (parameter.index / 32),
-                (1 shl parameter.index % 32).inv()
-            )
-        } else {
-            initializer("null")
-        }
-        if (parameter.isMandatory) {
-            addAnnotation(DslMandatory::class) {
-                useSiteTarget(AnnotationSpec.UseSiteTarget.SET)
-                addMember("group = %S", parameter.group)
+    private fun TypeSpecBuilder.addParameterProperty(parameter: Parameter, type: ClassName) =
+        addProperty(parameter.name, parameter.typeName.nullable) {
+            mutable(true)
+            if (parameter.hasDefault) {
+                delegate(
+                    "%1T.observable(null)·{·_, _, _·-> %2L·= %2L and %3L }",
+                    Delegates::class.asClassName(),
+                    DEFAULTS_BITFLAGS_FIELD_NAME + (parameter.index / 32),
+                    (1 shl parameter.index % 32).inv()
+                )
+            } else {
+                initializer("null")
             }
+            if (parameter.isMandatory) {
+                addAnnotation(DslMandatory::class) {
+                    useSiteTarget(AnnotationSpec.UseSiteTarget.SET)
+                    addMember("group = %S", parameter.group)
+                }
+            }
+            parameter.doc?.let { addKdoc(it) }
+            addKdoc("@see %T.%L", type, parameter.name)
         }
-        parameter.doc?.let { addKdoc(it) }
-        addKdoc("@see %T.%L", type, parameter.name)
-    }
 
 }
